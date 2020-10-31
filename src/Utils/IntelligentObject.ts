f6c6acb92279223af219340deca3823e829bb889
Jakub Sukiennik <kuba1207710@gmail.com>
2020-10-31 15:37:49 +0100

    adding InteligentObject class

--- src/Utils/IntelligentObject.ts
+++ src/Utils/IntelligentObject.ts
@@ -20,3 +20,21 @@ class IntelligentObject<T extends Object, U extends keyof T> {
 }
 
 export default IntelligentObject;
+
+export function IntelObj<T extends Object, U extends keyof T>(
+  inititalValues: T
+) {
+  function onChange(target: T, property: U, value: T[U]) {
+    if (!target.hasOwnProperty(property)) throw Error("Cannot add new keys");
+    target[property] = value;
+    return true;
+  }
+  function get(target: T, property: U) {
+    return target[property];
+  }
+
+  return new Proxy(inititalValues, {
+    set: onChange,
+    get,
+  });
+}
